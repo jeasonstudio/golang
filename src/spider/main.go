89b7bfd142997c886d6a9dce458150745e32a198
赵吉彤 <zjtong3576@sina.com>
2016-12-26 00:36:02 +0800

    sit

--- src/spider/main.go
+++ src/spider/main.go
@@ -47,21 +47,22 @@ func getAll(tagCookie string) {
 }
 
 func main() {
-	tagLoginURL := "http://zhiyuan.ustb.edu.cn/app.VPClient/index.jsp?m=vpclient&c=user&a=login"
+	tagLoginURL := "http://bj.lianjia.com/api/newhouserecommend"
 
 	v := url.Values{}
-	v.Set("username", "41524122")
-	v.Set("password", "060016")
-	v.Set("lastUrl", "")
+	v.Set("type", "1")
+	v.Set("query", "http://bj.lianjia.com/ershoufang/rs海淀/")
+	// v.Set("lastUrl", "")
 	body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", tagLoginURL, body)
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value") //这个一定要加，不加form的值post不过去，被坑了两小时
+	// req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value") //这个一定要加，不加form的值post不过去，被坑了两小时
 	// fmt.Printf("%+v\n", req)                                                         //看下发送的结构
-	// req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36")
-	req.Header.Set("Content-Length", "42")
-	// req.Header.Set("Cookie", "JSESSIONID=C010D246699DC59E6C29F0C2932244B3; _ga=GA1.3.1043019656.1481126573; JSESSIONID=00AA0A5E7C5C4B3FBECF9E9350594A1A")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36")
+	// req.Header.Set("Content-Length", "42")
+	// req.Header.Set("Cookie", "lianjia_uuid=4242529c-385e-4683-b572-e7cc89a9039d; select_city=110000; all-lj=80b391239fd880f59f779618fca39507; _gat=1; _gat_past=1; _gat_global=1; _gat_new_global=1; _gat_dianpu_agent=1; logger_session=0e3961d782a43dd6c22a3692833594e9; _ga=GA1.2.1571583035.1482682480; CNZZDATA1253477573=208171418-1482677452-null%7C1482677452; _smt_uid=585ff06e.4d27e347; CNZZDATA1254525948=1872321740-1482680111-null%7C1482680111; CNZZDATA1255633284=1260721563-1482681473-null%7C1482681473; CNZZDATA1255604082=661565453-1482678657-null%7C1482678657; lianjia_ssid=e2a2989f-85d0-49ed-9b51-4cab4476f14a")
+	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
 
 	resp, err := client.Do(req) //发送
 	defer resp.Body.Close()     //一定要关闭resp.Body
@@ -70,12 +71,6 @@ func main() {
 	if err != nil {
 		return
 	}
-	// tagCookie := strings.Split(strings.Split(resp.Header["Set-Cookie"][0], ";")[0], "=")[1]
-
-	// fmt.Println(resp.Header["Set-Cookie"], err)
 	fmt.Println(resp.StatusCode)
-
-	// getAll(tagCookie)
-	// fmt.Println(resp.Header["Set-Cookie"][0])
 
 }
